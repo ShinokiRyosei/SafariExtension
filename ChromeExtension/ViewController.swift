35f9804e4a1cbfb7d0523d57ff790bdc1ba12961
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-04-13 20:22:43 +0900

    initial commit

---
+++ ChromeExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ChromeExtension
+//
+//  Created by ShinokiRyosei on 2017/04/13.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - ViewController
+
+class ViewController: UIViewController {
+
+
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+    }
+}
+
